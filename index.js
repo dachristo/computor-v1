9c94b206f7e84d800699d8ec58b43cadc71dade7
dchristo <dchristo@student.42.fr>
2018-01-04 18:55:37 +0100

    degrée 1 et degrée 3 gérer, faire des tests pour les cas spéciaux.

--- index.js
+++ index.js
@@ -36,10 +36,14 @@ let the_sign_is = function(i, expression) {
             return (expression [i - 1]);
     else if (expression[i - 3] === "+" || expression[i - 3] === "-")
         return (expression [i - 3]);
+    else
+        return ("+");
 }
 
 let modify_expression = function(number, letter, sign, expression, i_save) {
     if (expression[i_save] == 0)
+        return ;
+    if (letter == undefined)
         return ;
     for (let i = 0; expression[i] && expression[i] !== "="; i++) {
         if (regex_find(i, expression) === letter) {
@@ -90,27 +94,39 @@ let modify_expression = function(number, letter, sign, expression, i_save) {
         else
             expression.splice(i_save - 3, 3);
     }
+}
 
+let find_if_degree_3 = function(expression) {
+    let regex_3 = /^X\^3$/;
+    for (let i = 0; expression[i] && expression[i] !== "="; i++) {
+        if (regex_3.test(expression[i]))
+            return true;
+    }
+    return false;
 }
 
 let regex_test = function(i, expression, abc){
-    let regex_a = /X\^2/;
-    let regex_b = /X\^1/;
-    let regex_c = /X\^0/;
+    let regex_a = /^X\^2$/;
+    let regex_b = /^X\^1$/;
+    let regex_c = /^X\^0$/;
 
-    if (regex_a.test(expression[i])) {
+    if (regex_a.test(expression[i]))
         abc[0] = the_number_is(i, expression);
-        console.log("a", abc[0]);
-    }
-    if (regex_b.test(expression[i])) {
+    if (regex_b.test(expression[i]))
         abc[1] = the_number_is(i, expression);
-        console.log("b", abc[1]);
-    }
-    if (regex_c.test(expression[i])) {
+    if (regex_c.test(expression[i]))
         abc[2] = the_number_is(i, expression);
-        console.log("c", abc[2]);
-    }
 }
+
+let find_solution_degree_1 = function(expression) {
+    let regex_1 = /^X\^1$/;
+
+    for (let i = 0; expression[i] && expression[i] !== "="; i++) {
+        if (regex_1.test(expression[i])) {
+            return (the_sign_is(i, expression) === "+" ? the_number_is(i, expression) * -1 : the_number_is(i, expression));
+        }
+    }
+};
 
 let main = function() {
     let abc = [];
@@ -123,26 +139,35 @@ let main = function() {
         let i = 0;
         while (expression[i] !== "=")
             i++;
-        i++;
+        let i_save = i++;
         while (expression[i]) {
             modify_expression(the_number_is(i, expression), regex_find(i, expression), the_sign_is(i, expression), expression, i);
             i++;
         }
+        expression.splice(i_save + 2, expression.length);
         for (i = 0; expression[i] && expression[i] !== "="; i++) {
             regex_test(i, expression, abc);
         }
-        console.log(expression.join(" "));
-        let delta = abc[1] * abc[1] - 4 * abc[0] * abc[2];
-        if (delta < 0)
-            console.log("Aucune solution n'est possible");
-        else if (delta === 0) {
-            console.log("Polynomial degree: 1\nThe solution is:")
-            console.log(-abc[1] / 2 * abc[0]);
+        console.log("Reduced form:", expression.join(" "));
+        if (find_if_degree_3(expression))
+            console.log("Polynomial degree: 3\nThe polynomial degree is stricly greater than 2, I can't solve.");
+        else if (abc[0] == undefined) {
+            console.log("Polynomial degree: 1\nThe solution is:");
+            console.log(1 / find_solution_degree_1(expression));
         }
-        else if (delta > 0) {
-            console.log("Polynomial degree: 2\nDiscriminant is strictly positive, the two solutions are:");
-            console.log((-abc[1] + Math.sqrt(delta)) / (2 * abc[0]));
-            console.log((-abc[1] - Math.sqrt(delta)) / (2 * abc[0]));
+        else {
+            let delta = abc[1] * abc[1] - 4 * abc[0] * abc[2];
+            if (delta < 0)
+                console.log("Aucune solution n'est possible");
+            else if (delta === 0) {
+                console.log("Polynomial degree: 1\nThe solution is:")
+                console.log(-abc[1] / 2 * abc[0]);
+            }
+            else if (delta > 0) {
+                console.log("Polynomial degree: 2\nDiscriminant is strictly positive, the two solutions are:");
+                console.log((-abc[1] + Math.sqrt(delta)) / (2 * abc[0]));
+                console.log((-abc[1] - Math.sqrt(delta)) / (2 * abc[0]));
+            }
         }
     }
 }
